f681759ef0f6911735aa7d84de1f0490a8614864
Jhohan B <jhohantma@gmail.com>
2025-06-09 22:19:44 -0500

    Se añaden alertas personalizadas, se quitan recargas de página y ciclos innecesarios, se renombra la finción de quitar por ocultar.

--- actividades/actividadTres/js/index.js
+++ actividades/actividadTres/js/index.js
@@ -1,5 +1,4 @@
 var obtenerDatos = () => {
-
   var datos = {
     nombre: document.getElementById("nombre").value,
     nombreDos: document.getElementById("nombreDos").value,
@@ -8,89 +7,84 @@ var obtenerDatos = () => {
     anioNac: parseInt(document.getElementById("anioNac").value),
     profesion: document.getElementById("profesion").value,
   };
-
   validacionDatos(datos);
-
 };
 
 var validacionDatos = (objeto) => {
-
-  var tarjeta = document.getElementById("tarjeta");
-  
   if (
     objeto.nombre == "" ||
     objeto.apellido == "" ||
     objeto.anioNac == "" ||
     objeto.profesion == ""
   ) {
-    alert("Ingresa todos los espacios requeridos.");
+    alerta("Ingresa todos los campos obligatorios.", "error");
   } else if (!Number.isInteger(objeto.anioNac)) {
-    alert("Ingresa un año entre 1920 y 2025 para el año de nacimiento, formato números, sin comas o puntos.");
-  } else if (objeto.anioNac <= 1920 || objeto.anioNac > 2025) {
-    alert("Ingresa un año de nacimiento válido.");
-  } else if(tarjeta.classList.contains("ver")){
-    alert("Intenta nuevamente");
-    location.reload();
-  } else{ reflejarDatos(objeto, edadUsuario(objeto)); 
-  };
+    alerta("Ingresa un año entre 1900 y 2025 para el año de nacimiento.");
+  } else if (objeto.anioNac < 1900 || objeto.anioNac > 2025) {
+    alerta("Ingresa un año de nacimiento válido.", "error");
+  } else if (edadUsuario(objeto) < 18) {
+    alerta(
+      "No puedes generar la tarjeta, ya que eres menor de edad.",
+      "error"
+    );
+  } else {
+    limpiarCampos();
+    limpiarCarta();
+    reflejarDatos(objeto, edadUsuario(objeto));
+  }
+};
 
+var limpiarCampos = () => {
+  document.getElementById("nombre").value = "";
+  document.getElementById("nombreDos").value = "";
+  document.getElementById("apellido").value = "";
+  document.getElementById("apellidoDos").value = "";
+  document.getElementById("anioNac").value = "";
+  document.getElementById("profesion").value = "";
+};
+
+var limpiarCarta = (objeto) => {
+  document.getElementById("cardNombre").innerHTML = "";
+  document.getElementById("cardApellido").innerHTML = "";
+  document.getElementById("cardTexto").innerHTML = "";
 };
 
 var edadUsuario = (objeto) => {
-
   var fecha = new Date();
   var anioActual = fecha.getFullYear();
   var edadActual = anioActual - objeto.anioNac;
   return edadActual;
-
 };
 
 var reflejarDatos = (objeto, edadActual) => {
-
-  if (edadActual < 18) {
-    alert("No puedes generar la tarjeta ya que eres menor de edad.");
-  } else {
-    var texto = `Hola, soy ${objeto.profesion} y tengo ${edadActual} años. Esta es mi carta
+  var texto = `Hola, soy ${objeto.profesion} y tengo ${edadActual} años. Esta es mi carta
                  de presentación.`;
+  const arrayIny = ["cardNombre", "cardApellido", "cardTexto"];
 
-    const arrayIny = ["cardNombre", "cardApellido", "cardTexto"];
+  document.getElementById("cardNombre").innerHTML += `${objeto.nombre} ${objeto.nombreDos}`;
+  document.getElementById("cardApellido").innerHTML += `${objeto.apellido} ${objeto.apellidoDos}`;
+  document.getElementById("cardTexto").innerHTML += texto;
 
-    for (var i = 0; i < arrayIny.length; i++) {
-      switch (arrayIny[i]) {
-        case "cardNombre":
-          document.getElementById(
-            "cardNombre"
-          ).innerHTML += `${objeto.nombre} ${objeto.nombreDos}`;
-          break;
-
-        case "cardApellido":
-          document.getElementById(
-            "cardApellido"
-          ).innerHTML += `${objeto.apellido} ${objeto.apellidoDos}`;
-          break;
-
-        case "cardTexto":
-          document.getElementById("cardTexto").innerHTML += texto;
-          break;
-      }
-    }
-
-    encendido();
-  }
-
+  encendido();
+  alerta("Tarjeta generada", "success");
 };
 
 var encendido = () => {
-
   var claseTarjeta = document.getElementById("tarjeta");
   claseTarjeta.classList.remove("ocultar");
   claseTarjeta.classList.add("ver");
-
 };
 
-var quitar = () => {
-
+var ocultar = () => {
   var claseTarjeta = document.getElementById("tarjeta");
-  location.reload();
+  claseTarjeta.classList.remove("ver");
+  claseTarjeta.classList.add("ocultar");
+};
 
+var alerta = (mensaje, icono) => {
+  Swal.fire({
+    title: mensaje,
+    icon: icono,
+    draggable: true,
+  });
 };
